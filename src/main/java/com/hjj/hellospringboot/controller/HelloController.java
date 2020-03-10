b976993a512086ff44d46b3c36f53533dd2ee5c3
qiang <279847270@qq.com>
2020-03-10 09:16:07 +0800

    dev

---
+++ src/main/java/com/hjj/hellospringboot/controller/HelloController.java
@@ -0,0 +1,17 @@
+package com.hjj.hellospringboot.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @author hjj
+ * @create 2020-03-07 17:59
+ */
+@RestController
+public class HelloController {
+
+    @RequestMapping("/hello")
+    public String hell() {
+        return "hello spring boot this is dev";
+    }
+}
